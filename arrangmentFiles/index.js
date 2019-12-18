e0f8374f16a8f1b242b28f125cca98e318849e02
zhus-dika <zhus.dinara@gmail.com>
2019-12-18 23:32:24 +0600

    added comments

--- arrangmentFiles/index.js
+++ arrangmentFiles/index.js
@@ -5,12 +5,14 @@ const readFiles = require('../readFiles');
 const newDir = './temp';
 module.exports = (base) => {
     var files = readFiles(base, 0);
+    /****create new dir for files***/
     mkdirp(newDir, function (err) {
         if (err) console.error(err);
     });
     /**************** arrangment file objects *******************/
     files.forEach(item => { 
         let nameDir = path.join(newDir, path.basename(item).substr(0,1).toUpperCase());
+        /****create new dir for sorting files ***/
         mkdirp(nameDir, function (err) {
             if (err) console.error(err);
                 });
