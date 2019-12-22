932cb40ddd6239ad28faa5a20776f10243540250
zhus-dika <zhus.dinara@gmail.com>
2019-12-22 18:57:18 +0600

    1st ed: corrected argv params

--- main.js
+++ main.js
@@ -4,13 +4,33 @@ const arrangmentFiles = require('./arrangmentFiles');
 const deleteDirRec = require('./deleteDirRec');
 let sourceDir = './test'; //default value
 let targetDir = './new'; //default value
-if (process.argv[2] == '--sourceDir' && process.argv[4] == '--targetDir') {
-    sourceDir = process.argv[3];
-    targetDir = process.argv[5];
-}
-arrangmentFiles(sourceDir, targetDir);
-if (process.argv[6] == '--delete'){
-    deleteDirRec(sourceDir);
+let i = 2;
+let argv = process.argv[i];
+var flag = false;
+while (argv) {
+    switch (argv) {
+        case '--sourceDir': 
+        if(fs.existsSync(sourceDir)) {
+            sourceDir = process.argv[i + 1];
+        } else {
+            console.log(process.argv[i + 1] + " isn't exist");
+            flag = true;
+        }
+        i += 2;
+        break;
+        case '--targetDir': targetDir = process.argv[i + 1];
+        i += 2;
+        break;
+        case '--delete': deleteDirRec(sourceDir);
+        i++;
+        break;
+        default: 
+        console.log('incorrect parameter');
+        flag = true;
+        break;
+    }
+    if (flag) break;
+    argv = process.argv[i];
 }
 
 
