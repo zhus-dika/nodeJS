be5249b815bd2ef53f37f13d94b2e6e0d0bb5258
zhus-dika <zhus.dinara@gmail.com>
2019-12-23 13:46:59 +0600

    2d ed: corrected mkdir func

--- arrangmentFiles/index.js
+++ arrangmentFiles/index.js
@@ -12,9 +12,11 @@ module.exports = (base, newDir) => {
     files.forEach(item => { 
         let nameDir = path.join(newDir, path.basename(item).substr(0,1).toUpperCase());
         /****create new dir for sorting files ***/
-        mkdirp(nameDir, function (err) {
-            if (err) console.error(err);
-                });
+        if (!fs.existsSync(nameDir)) {
+            fs.mkdirSync(nameDir, function (err) {
+                if (err) console.error(err);
+                    });
+        }
         let nextPathFile = path.join(nameDir, path.basename(item));
         if (!fs.existsSync(nextPathFile)) {
             fs.link(item, nextPathFile , err => {
