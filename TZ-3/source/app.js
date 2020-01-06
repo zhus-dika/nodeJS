a5374e02ae30db271dae322ef9a060955e5d83d2
zhus-dika <zhus.dinara@gmail.com>
2020-01-06 18:15:50 +0600

    added error template pug

--- TZ-3/source/app.js
+++ TZ-3/source/app.js
@@ -1,3 +1,4 @@
+//nodemon --ignore models/ app.js
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -11,6 +12,19 @@ app.use(express.static('../public'))
 
 app.use('/', require('./routes/index'))
 app.use('/login', require('./routes/index'))
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error', { message: err.message, error: err })
+})
 
 const server = app.listen(process.env.PORT || 3000, function () {
   console.log('Сервер запущен на порте: ' + server.address().port)
